c6a025b01f4af7b4a8fd011690ea5f4d76ace2aa
mihneabranzeu <mihneacnitv@gmail.com>
2024-06-01 18:45:21 +0300

    changes

--- Backend/Aggregator/FirebaseStorageService.py
+++ Backend/Aggregator/FirebaseStorageService.py
@@ -1,3 +1,6 @@
+import json
+from typing import List, Tuple
+import base64
 from firebase_admin import credentials, initialize_app, storage
 
 class FirebaseStorageService:
@@ -7,17 +10,25 @@ class FirebaseStorageService:
 		
 		self.bucket = storage.bucket()
 
-	def downloadClientModels(self):
+	def downloadClientModels(self) -> List[Tuple[int, List[bytes]]]:
 		blobs = self.bucket.list_blobs(prefix="clientModels/mnist/client_mnist_model_")
+		parameters = []
 		for blob in blobs:
 			print("Blob name: " + blob.name)
-			# here should be a download happening
+			content = blob.download_as_string()
+			parameters_dict=json.loads(content)
+			# Maybe change this to include actual examples count
+			num_examples = 1
+			model_weights = [base64.b64decode(parameters_dict[key]) for key in sorted(parameters_dict.keys())]
+			parameters.append((num_examples, model_weights))
+		return parameters
 			
-	def uploadModel(self, modelName):
-		modelPath = f"aggregatedModels/mnist/{modelName}"
-		blob = self.bucket.blob(modelPath)
-		
-		content = "This is the content of the model from the Aggregator"
-		blob.upload_from_string(content) # this should be "upload from file"
+	def uploadModel(self, model_name, parameters: List[bytes]):
+		model_path = f"aggregatedModels/mnist/{model_name}"
+		blob = self.bucket.blob(model_path)
 
+		parameters_dict = {f"layer{i}": base64.b64encode(param).decode('utf-8') for i, param in enumerate(parameters)}
+		content = json.dumps(parameters_dict)
+
+		blob.upload_from_string(content) # this should be "upload from file"
 
